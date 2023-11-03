4b8c6ded6fd86969ec8375c976f354e76c8ad194
Mariam ElGhobary <M.ElGhobary@aucegypt.edu>
2023-11-04 01:41:06 +0200

    Updated ALU CU module and uploaded immGen and ALU modules

--- Modules/ALU_CU.v
+++ Modules/ALU_CU.v
@@ -22,22 +22,40 @@
 
 module ALU_CU(
 input [1:0] ALUop,
-input [2:0] inst_1,
-input inst_2,
+input [2:0] inst_1, //func3
+input inst_2, //func7
 output reg [3:0] ALU_sel
     );
     
     always @(*) begin
-    
-        if(ALUop == 0) ALU_sel = 4'b0010;
-        else if(ALUop == 2'b01) ALU_sel = 4'b0110;
-        else if(ALUop == 2'b10) begin
-            if (inst_1 == 0 && inst_2 == 0) ALU_sel = 4'b0010;
-            else if (inst_1 == 0 && inst_2 == 1) ALU_sel = 4'b0110;
-            else if (inst_1 == 3'b111 && inst_2 == 0) ALU_sel = 4'b0000;
-            else if (inst_1 == 3'b110 && inst_2 == 0) ALU_sel = 4'b0001;
+    case(ALUop) 
+        2'b00: ALU_sel = ALU_ADD; 
+        2'b01: ALU_sel = ALU_SUB;
+        2'b10: begin //R-Type and I-Type
+            case(inst_1)  
+                3'd0 : begin 
+                            if(inst_2 == 0)  
+                                ALU_sel = ALU_ADD; 
+                            else ALU_sel = ALU_SUB; 
+                       end
+                3'd4: ALU_sel = ALU_XOR;        ;
+                3'd6: ALU_sel = ALU_OR;
+                3'd7: ALU_sel = ALU_AND;
+                3'd1: ALU_sel = ALU_SLL;
+                3'd5: begin 
+                            if(inst_2 == 0) 
+                                ALU_sel = ALU_SRL;
+                            else ALU_sel = ALU_SRA;
+                      end
+                3'd2: ALU_sel = ALU_SLT;
+                3'd3: ALU_sel = ALU_SLTU;
+            endcase
         end
+        2'b11: ALU_sel = ALU_PASS; //lui and nop
+    endcase
+
     
     end
     
 endmodule
+
